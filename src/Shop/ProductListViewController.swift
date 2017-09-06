99c7536bb0810a9e9bd7e503e1f73de58f47df1d
Thomas Dubiel <thomas.dubiel@sap.com>
2017-09-06 10:51:20 +0200

    Adding a table view and a sample Cell to the Product View

--- src/Shop/ProductListViewController.swift
+++ src/Shop/ProductListViewController.swift
@@ -8,18 +8,54 @@
 
 import UIKit
 import SAPCommon
+import SAPFiori
 
 class ProductListViewController: UIViewController {
    
+   @IBOutlet weak var tableView: UITableView!
+   
    let logger = Logger.shared(named: "ProductListViewController")
+   let objectCellId = "ProductCellID"
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      tableView.estimatedRowHeight = 80
+      tableView.rowHeight = UITableViewAutomaticDimension
+      tableView.register(FUIObjectTableViewCell.self, forCellReuseIdentifier: objectCellId)
+   }
+   
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+extension ProductListViewController: UITableViewDataSource, UITableViewDelegate {
+   
+   // Table view data source
+   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      // return the number of rows
+      return 1
+   }
+   
+   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      let objectCell = tableView.dequeueReusableCell(withIdentifier: objectCellId,
+                                                     for: indexPath as IndexPath) as! FUIObjectTableViewCell
+      
+      objectCell.headlineText = "Speed Mouse"
+      objectCell.subheadlineText = "HT-1061"
+      objectCell.footnoteText = "Computer Components"
+      objectCell.descriptionText = "Optical USB, PS/2 Mouse, Color: Blue, 3-button-functionality (incl. Scroll wheel)"
+      objectCell.detailImage = UIImage() // TODO: Replace with your image
+      objectCell.detailImage?.accessibilityIdentifier = "Speed Mouse"
+      objectCell.statusText = "7.00 USD"
+      objectCell.substatusText = "In Stock"
+      objectCell.substatusLabel.textColor = .preferredFioriColor(forStyle: .positive)
+      objectCell.accessoryType = .disclosureIndicator
+      objectCell.splitPercent = CGFloat(0.4)
+      
+      return objectCell
+   }
+   
 }
